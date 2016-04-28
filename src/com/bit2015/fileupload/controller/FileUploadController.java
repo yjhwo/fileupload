c7be13bf479a5c25747d3e707cf9d2022098b004
yjhwo <yjhwo@naver.com>
2016-04-28 21:35:56 +0900

    File Upload

--- src/com/bit2015/fileupload/controller/FileUploadController.java
+++ src/com/bit2015/fileupload/controller/FileUploadController.java
@@ -9,14 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileUploadController {
 	
 	private static final Log LOG = LogFactory.getLog( FileUploadController.class );
-	private static final String SAVE_PATH = "/temp";
+	private static final String SAVE_PATH = "/temp";					// 미리 약속을 해둬야함
 	  
 	@RequestMapping( "/form" )
 	public String form() {
@@ -32,15 +31,15 @@ public class FileUploadController {
 		// 첫 번째 파일 처리
 		if( file1.isEmpty() == false ) {
 			
-	        String fileOriginalName = file1.getOriginalFilename();
+	        String fileOriginalName = file1.getOriginalFilename();		// 사용자가 업로드한 파일 이름.(ex. 지출.txt)
 	        String extName = fileOriginalName.substring( fileOriginalName.lastIndexOf(".") + 1, fileOriginalName.length() );
-	        String saveFileName = genSaveFileName( extName );
+	        String saveFileName = genSaveFileName( extName );			// 저장할 파일 이름(구분이 쉽게 보통 날짜로 많이 함) => 유일해야 함!
 	        Long size = file1.getSize();
 	        
-	        writeFile( file1, SAVE_PATH, saveFileName );
+	        writeFile( file1, SAVE_PATH, saveFileName );				// file1안에 InputStream이 들어가 있음
 
-	        String url = "/product-images/" + saveFileName;
-	        model.addAttribute( "productImageUrl1", url );
+	        String url = "/product-images/" + saveFileName;				// ★★★ 저장한 경로를 주지않고 쌩뚱맞은 폴더 이름으로 해놓은 이유는
+	        model.addAttribute( "productImageUrl1", url );				// spring-servlet참조, mapping시켜 놓음
 	
 	        LOG.debug( " ######## fileOriginalName : " + fileOriginalName );
 	        LOG.debug( " ######## fileSize : " + size );
@@ -54,7 +53,7 @@ public class FileUploadController {
 	private void writeFile( MultipartFile file, String path, String fileName ) {
 		FileOutputStream fos = null;
 		try {
-			byte fileData[] = file.getBytes();
+			byte fileData[] = file.getBytes();					// getBytes():업로드한 파일 데이터를 구하는 가장 단순한 방법
 			fos = new FileOutputStream( path + "/" + fileName );
 			fos.write(fileData);
 		} catch (Exception e) {
